5fe988abcc2fed78740b87eb16ecca6037b1db0f
p-fisher <93838443+p-fisher@users.noreply.github.com>
2021-12-16 15:18:39 -0500

    adding js name change

---
+++ js-profile-page/script.js
@@ -0,0 +1,16 @@
+function editProfile(id){
+    var name = prompt("What name would you like on your profile?");
+    document.getElementById(id).innerText = name;
+    // return name;
+    // var greeting=`${name} here. Just wanted to say Hello World`;
+    // var steveGreeting="Steve here, AKA the Stevenator. Just wanted to say Hello World";
+    // // prompt ("What is your name?");
+    // if (name === "Steve") {
+    //     console.log(steveGreeting)
+    // } else {
+    //     console.log(greeting);
+    // }
+}
+// editProfile();
+
+
