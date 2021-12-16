2753c2adf505dd904cc01dd853af9df69dee58d5
p-fisher <93838443+p-fisher@users.noreply.github.com>
2021-12-15 23:28:01 -0500

    fin

--- js-liking-likes/script.js
+++ js-liking-likes/script.js
@@ -1,10 +1,38 @@
-var likeNum=0;
-var likeString = document.querySelector("#likeCount")
-//
-// console.log(likeString);
-//
-function addLike() {
-    likeNum++;
-    likeString.innerText = likeNum + " Like(s)"
-    // console.log(likeNum);
+// var likeNum=0;
+// var likeString = document.querySelector("#likeCount")
+// //
+// // console.log(likeString);
+// //
+// function addLike() {
+//     likeNum++;
+//     likeString.innerText = likeNum + " Like(s)"
+//     // console.log(likeNum);
+// }
+
+
+// part II
+
+var likeNum1=0;
+var likeString1 = document.querySelector("#likeCount1")
+
+var likeNum2=0;
+var likeString2 = document.querySelector("#likeCount2")
+
+var likeNum3=0;
+var likeString3 = document.querySelector("#likeCount3")
+
+
+function addLike1() {
+    likeNum1++
+    likeString1.innerText = likeNum1 + " Like(s)"
+}
+
+function addLike2() {
+    likeNum2++
+    likeString2.innerText = likeNum2 + " Like(s)"
+}
+
+function addLike3() {
+    likeNum3++
+    likeString3.innerText = likeNum3 + " Like(s)"
 }
