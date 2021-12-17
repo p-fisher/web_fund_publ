e535edafd6412253460870b2a8596253b0c0c136
p-fisher <93838443+p-fisher@users.noreply.github.com>
2021-12-17 00:19:26 -0500

    mostly there but for request02

--- js-profile-page/script.js
+++ js-profile-page/script.js
@@ -1,16 +1,41 @@
+// allows user to edit their name shown on profile incl. prompt for what they want 
 function editProfile(id){
     var name = prompt("What name would you like on your profile?");
     document.getElementById(id).innerText = name;
-    // return name;
-    // var greeting=`${name} here. Just wanted to say Hello World`;
-    // var steveGreeting="Steve here, AKA the Stevenator. Just wanted to say Hello World";
-    // // prompt ("What is your name?");
-    // if (name === "Steve") {
-    //     console.log(steveGreeting)
-    // } else {
-    //     console.log(greeting);
-    // }
 }
-// editProfile();
 
+// removes the request PER DIV ROW whether clicking either button
+function removeRequest(id) {
+    document.getElementById(id).remove();
+}
 
+// decreases count on requests upon clicking either button
+var countNum = document.querySelector("#request-count").innerText;
+var currentCount = document.querySelector("#request-count")
+function decreaseCount(id) {    
+    countNum--;
+    currentCount.innerText = countNum;
+}
+
+// remove div row and decrease count
+function requestsCleanupMinus() {
+    var arr = ['request01','request-count']
+    removeRequest(arr[0]);
+    decreaseCount(arr[1]);
+}
+
+// add to count of total connections upon accepting request
+var countConnNum = document.querySelector("#connections-count").innerText;
+var currentConnCount = document.querySelector("#connections-count")
+function increaseConnCount(id) {    
+    countConnNum++;
+    currentConnCount.innerText = countConnNum;
+}
+
+// removes div row and count in requests while adding to total count of connections
+function requestsCleanupPlus() {
+    var arr2 = ['request01','request-count','connections-count']
+    removeRequest(arr2[0]);
+    decreaseCount(arr2[1]);
+    increaseConnCount(arr2[2]);
+}
