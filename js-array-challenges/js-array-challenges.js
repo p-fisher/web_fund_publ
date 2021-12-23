5a1d32b454a661d24c7f0a09a62f37194c8b2600
p-fisher <ops.patrickfisher@gmail.com>
2021-12-22 23:15:04 -0500

    begin fibonacci excs

--- js-array-challenges/js-array-challenges.js
+++ js-array-challenges/js-array-challenges.js
@@ -76,4 +76,13 @@ console.log(result); // we expect back ["e", "d", "c", "b", "a"]
 
 //---------------------------------------------------------------------------------
 
-//
+//Fibonacci array
+function fibonacciArray(n) {
+   // the [0, 1] are the starting values of the array to calculate the rest from
+   var fibArr = [0, 1];
+   // your code here
+   return fibArr;
+}
+  
+var result = fibonacciArray(10);
+console.log(result); // we expect back [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
